f687817ebe0884d0ecbb9b5d2568c9a7c99e4c79
Stephen Schwenck <sschwenck@regis.edu>
2014-03-23 23:34:50 -0600

    udating models and design doc

--- OrderEntry/Models/LineViewModel.cs
+++ OrderEntry/Models/LineViewModel.cs
@@ -8,15 +8,43 @@ namespace OrderEntry.Models
       public OrderViewModel Order { get; set; }
 
       [Required]
+      [Display(Name = "Line Number")]
+      public int LineNo { get; set; }
+
+      [Required]
       [Display(Name = "Product")]
       public string Product { get; set; }
 
       [Required]
-      [Display(Name = "Quantity")]
-      public int Quantity { get; set; }
+      [Display(Name = "Description")]
+      public string Description { get; set; }
+
+      [Required]
+      [Display(Name = "Quantity Ordered")]
+      public int QtyOrd { get; set; }
+
+      [Display(Name = "Quantity Shipped")]
+      public int QtyShip { get; set; }
 
       [Display(Name = "Price")]
       public decimal Price { get; set; }
 
+      [Display(Name = "Discount")]
+      public decimal Discount { get; set; }
+
+      [Display(Name = "Net Amount")]
+      public decimal NetAmt { get; set; }
+
+      [Display(Name = "Margin Amount")]
+      public decimal MarginAmt { get; set; }
+
+      [Display(Name = "Margin Percent")]
+      public decimal MarginPct { get; set; }
+
+      [Display(Name = "Net Available")]
+      public int NetAvail { get; set; }
+
+      [Display(Name = "Unit")]
+      public string Unit { get; set; }
    }
 }
--- OrderEntry/Models/OrderViewModel.cs
+++ OrderEntry/Models/OrderViewModel.cs
@@ -13,18 +13,16 @@ namespace OrderEntry.Models
       public int OrderSuffix { get; set; }
 
       [Required]
-      //TODO: make this be in xxx-xx format
-      //[RegularExpression()]
       [Display(Name = "Order Type")]
       public string OrderType { get; set; }
 
       [Required]
       [Display(Name = "Warehouse")]
-      public int Warehouse { get; set; }
+      public WarehouseModel Warehouse { get; set; }
 
       [Required]
       [Display(Name = "Customer")]
-      public int Customer { get; set; }
+      public CustomerModel Customer { get; set; }
 
       [Display(Name = "Customer PO")]
       public string CustomerPO { get; set; }
@@ -33,6 +31,74 @@ namespace OrderEntry.Models
       public string TakenBy { get; set; }
 
       [Display(Name = "Ship To")]
-      public int ShipTo { get; set; }
+      public ShipToModel ShipTo { get; set; }
+
+      [Display(Name = "Taxable")]
+      public bool Taxable { get; set; }
+
+      [Display(Name = "Number of Lines")]
+      public int Lines { get; set; }
+
+      [Display(Name = "Order Total")]
+      public decimal OrderTotal { get; set; }
+
+      [Display(Name = "Tendered Amount")]
+      public decimal TenderedAmount { get; set; }
+   }
+
+   public class CustomerModel
+   {
+      [Required]
+      [Display(Name = "Customer")]
+      public int Customer { get; set; }
+
+      [Display(Name = "Address")]
+      public AddressModel Address { get; set; }
+   }
+
+   public class WarehouseModel
+   {
+      [Required]
+      [Display(Name = "Warehouse")]
+      public int Warehouse { get; set; }
+
+      [Display(Name = "Address")]
+      public AddressModel Address { get; set; }
+   }
+
+   public class ShipToModel
+   {
+      [Required]
+      [Display(Name = "Ship To Number")]
+      public int ShipToNo { get; set; }
+
+      [Display(Name = "Address")]
+      public AddressModel Address { get; set; }
+   }
+
+   public class AddressModel
+   {
+      [Required]
+      [Display(Name = "Address Line 1")]
+      public string AddressLine1 { get; set; }
+
+      [Display(Name = "Address Line 2")]
+      public string AddressLine2 { get; set; }
+
+      [Required]
+      [Display(Name = "City")]
+      public string City { get; set; }
+
+      [Required]
+      [Display(Name = "State")]
+      public string State { get; set; }
+
+      [Required]
+      [Display(Name = "Zip Code")]
+      public int ZipCode { get; set; }
+
+      [Required]
+      [Display(Name = "Country")]
+      public string Country { get; set; }
    }
 }
